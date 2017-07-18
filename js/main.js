53f93d500a4894f1d228d6616471bcfc114d4c17
Steffany Bahamon <wwn677@dca90475f204.cof.ds.capitalone.com>
2017-07-18 14:26:42 -0400

    create4d game logic

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and Running!");
+
+
+//initialize cards
+var card1 = "queen";
+var card2 = "queen";
+var card3 = "king";
+var card4 = "king";
+
+console.log("User flipped" + card1);
+console.log("User flipped" + card2);
