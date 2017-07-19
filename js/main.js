de14d6e7745cf06766c1d4a79761235634c02908
Steffany Bahamon <wwn677@dca90475f204.cof.ds.capitalone.com>
2017-07-19 11:28:26 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 console.log("Up and Running!");
 
-
 //initialize cards
-var card1 = "queen";
-var card2 = "queen";
-var card3 = "king";
-var card4 = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped" + card1);
-console.log("User flipped" + card2);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flippled " + cardsInPlay);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flippled " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2){
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You Found A Match!");
+  } else{
+    alert("Sorry, Try Again.");
+  }
+}
+
+console.log(cardsInPlay)
